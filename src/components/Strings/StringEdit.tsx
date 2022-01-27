8a6431939915535077a7bbfd284c55621a211e87
Zulfiqar Ali <01zulfiqarali@gmail.com>
2022-01-27 21:20:02 +0500

    Start writing StringEdit component

---
+++ src/components/Strings/StringEdit.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react';
+
+interface StringEditProps {
+  data: any;
+}
+
+const StringEdit: FC<StringEditProps> = function StringEdit({ data }) {};
+
+export default StringEdit;
