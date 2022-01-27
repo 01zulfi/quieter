a0a2cb03e44ad879a664097757ce7384f1d12899
Zulfiqar Ali <01zulfiqarali@gmail.com>
2022-01-27 14:02:48 +0500

    Add tests for Modal component

---
+++ src/tests/Modal.test.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+import Modal from '../components/Modal';
+
+describe('tests modal component', () => {
+  test('renders children correctly', () => {
+    const { getByRole, getByPlaceholderText } = render(
+      <Modal closeModal={() => {}}>
+        <h1>Testing modal</h1>
+        <input type="text" placeholder="testing input" />
+      </Modal>,
+    );
+    const heading = getByRole('heading', { name: 'Testing modal' });
+    const input = getByPlaceholderText('testing input');
+
+    expect(heading).toBeInTheDocument();
+    expect(input).toBeInTheDocument();
+  });
+
+  test('invokes close modal function', () => {
+    const closeModalMock = jest.fn();
+    const { getByRole } = render(<Modal closeModal={closeModalMock} />);
+
+    const closeButton = getByRole('button', { name: 'Close' });
+    userEvent.click(closeButton);
+
+    expect(closeModalMock).toHaveBeenCalledTimes(1);
+  });
+});
