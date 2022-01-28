b6fb5608c1a863770070349d194cb7e7d60a2b9f
Zulfiqar Ali <01zulfiqarali@gmail.com>
2022-01-28 17:53:33 +0500

    Add BoxContext.tsx

---
+++ src/context/BoxContext.tsx
@@ -0,0 +1,8 @@
+import { createContext, useContext } from 'react';
+
+const BoxContext = createContext(null);
+
+const useBox = useContext(BoxContext);
+
+export default BoxContext;
+export { useBox };
