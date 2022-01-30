9c3c20c9cf93edaede6a3fca866beb9f556f1ee2
Zulfiqar Ali <01zulfiqarali@gmail.com>
2022-01-30 16:45:35 +0500

    Add UserInterface.ts

---
+++ src/interfaces/UserInterface.ts
@@ -0,0 +1,11 @@
+interface UserInterface {
+  adminBoxes?: string[];
+  associatedStrings?: string[];
+  authoredKnots?: string[];
+  authoredStrings?: string[];
+  joinedBoxes?: string[];
+  id: string;
+  username: string;
+}
+
+export default UserInterface;
