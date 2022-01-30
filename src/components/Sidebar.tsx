70621f59d078ef8c3d53f748b4caedf50b60f21e
Zulfiqar Ali <01zulfiqarali@gmail.com>
2022-01-30 18:32:29 +0500

    Add Sidebar component; incommplete

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+
+const Sidebar: FC = function Sidebar() {
+  return (
+    <section>
+      <div>
+        <h3>Explore Boxes</h3>
+      </div>
+    </section>
+  );
+};
+
+export default Sidebar;
