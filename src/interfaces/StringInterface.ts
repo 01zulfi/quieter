180ad0e9159d3e21e07f8b7d832a3dadad2e42de
Zulfiqar Ali <01zulfiqarali@gmail.com>
2022-01-30 15:48:24 +0500

    Add StringInteface.ts

---
+++ src/interfaces/StringInterface.ts
@@ -0,0 +1,12 @@
+interface StringInterface {
+  associatedBox: string;
+  hasKnots: boolean;
+  associatedKnots?: string[];
+  author: string;
+  associatedUsers: string[];
+  id: string;
+  title: string;
+  content: string;
+}
+
+export default StringInterface;
