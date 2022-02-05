ad5477acd7e59dca78887fadcc9d8d93efb22701
Zulfiqar Ali <01zulfiqarali@gmail.com>
2022-02-05 15:38:17 +0500

    Fix bug in useBox hook

--- src/context/BoxContext.tsx
+++ src/context/BoxContext.tsx
@@ -2,7 +2,7 @@ import { createContext, useContext } from 'react';
 
 const BoxContext = createContext(null);
 
-const useBox = useContext(BoxContext);
+const useBox = () => useContext(BoxContext);
 
 export default BoxContext;
 export { useBox };
