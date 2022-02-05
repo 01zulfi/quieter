22fdfa30581c970f69ae6239a14a417db41f8ff4
Zulfiqar Ali <01zulfiqarali@gmail.com>
2022-02-05 12:59:49 +0500

    Add tests for StringDelete component

---
+++ src/tests/Strings/StringDelete.test.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { waitFor, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+import StringDelete from '../../components/Strings/StringDelete';
+
+const mockDeleteString = jest.fn();
+
+jest.mock('../../context/StringContext', () => ({
+  useString: () => ({
+    id: '0123',
+  }),
+}));
+
+jest.mock('react-router-dom', () => ({
+  Link: function Link({ to, children }: { to: string; children: any }) {
+    return <a href={to}>{children}</a>;
+  },
+}));
+
+jest.mock('../../utils/firebase', () => ({
+  deleteString: (id: string) => mockDeleteString(id),
+}));
+
+describe('tests StringDelete component', () => {
+  it('renders string has been deleted text when button clicks', async () => {
+    render(<StringDelete />);
+    const deleteButton = screen.getByRole('button');
+
+    userEvent.click(deleteButton);
+
+    const heading = await screen.findByRole('heading', {
+      name: 'the string has been deleted',
+    });
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('invokes deleteString firebase method with string id', async () => {
+    render(<StringDelete />);
+    const deleteButton = screen.getByRole('button');
+
+    userEvent.click(deleteButton);
+
+    await waitFor(() => expect(mockDeleteString).toHaveBeenCalledWith('0123'));
+  });
+});
