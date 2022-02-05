be27b0ccdf9db8e48be23b7fcf82b038795836cb
Zulfiqar Ali <01zulfiqarali@gmail.com>
2022-02-05 14:54:21 +0500

    Fix bugs in StringContainer component

--- src/components/Strings/StringContainer.tsx
+++ src/components/Strings/StringContainer.tsx
@@ -6,9 +6,12 @@ import StringAuthorView from './StringAuthorView';
 import String from './String';
 import Knot from '../Knots/Knot';
 import KnotCreate from '../Knots/KnotCreate';
+import Loading from '../Loading';
+import firebase from '../../utils/firebase';
 
 const StringContainer: FC = function StringContainer() {
   const params = useParams();
+  const [isLoaded, setIsLoaded] = useState(false);
   const [string, setString] = useState<any>({});
   const user = useUser();
 
@@ -16,29 +19,35 @@ const StringContainer: FC = function StringContainer() {
     (async () => {
       const stringData = await firebase.getString(params.stringId);
       setString(stringData);
+      setIsLoaded(true);
     })();
   }, []);
 
-  const isCurrentUserAuthor = user.authoredStrings.find(
+  const isCurrentUserAuthor = user.authoredStrings.some(
     (id: string) => id === params.stringId,
   );
 
+  if (!isLoaded) return <Loading />;
+
   return (
-    <div>
+    <section>
       <StringContext.Provider value={string}>
         {isCurrentUserAuthor && <StringAuthorView />}
+
         <div>
           <String />
-          {string.knots.map((knotId: string) => (
-            <div key={knotId}>
-              <Knot knotId={knotId} />
-            </div>
-          ))}
+
+          {string.associatedKnots.length > 0 &&
+            string.associatedKnots.map((knotId: string) => (
+              <div key={knotId}>
+                <Knot knotId={knotId} />
+              </div>
+            ))}
+
           <KnotCreate />
         </div>
-        )
       </StringContext.Provider>
-    </div>
+    </section>
   );
 };
 
