38a712c93453a843ded0e31417d41eb3d016b102
Zulfiqar Ali <01zulfiqarali@gmail.com>
2022-02-06 20:36:58 +0500

    Add isUserSignedIn state in SignInPage - Reason: to conditionally render SignInForm

--- src/components/AuthComponents/SignInPage.tsx
+++ src/components/AuthComponents/SignInPage.tsx
@@ -1,11 +1,27 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
+import firebase from '../../utils/firebase';
 import SignInForm from './SignInForm';
 
 const SignInPage: FC = function SignInPage() {
+  const [isUserSignedIn, setIsUserSignedIn] = useState(false);
+
+  useEffect(() => {
+    const setStateAfterSignIn =
+      firebase.updateStateUponSignIn(setIsUserSignedIn);
+    setStateAfterSignIn(true);
+  }, []);
+
   return (
     <section>
       <h1>Sign in to quieter</h1>
-      <SignInForm />
+      {isUserSignedIn ? (
+        <div>
+          <h2>Already signed in</h2>
+          <p>Go to homepage</p>
+        </div>
+      ) : (
+        <SignInForm />
+      )}
     </section>
   );
 };
