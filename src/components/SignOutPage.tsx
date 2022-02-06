4ce687865f09e32ced2ccddf4fe54b0350df8f70
Zulfiqar Ali <01zulfiqarali@gmail.com>
2022-02-06 13:22:53 +0500

    Add SignOutPage component

---
+++ src/components/SignOutPage.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+
+const SignOutPage: FC = function SignOutPage() {
+  return (
+    <section>
+      <h2>successfully signed out</h2>
+    </section>
+  );
+};
+
+export default SignOutPage;
