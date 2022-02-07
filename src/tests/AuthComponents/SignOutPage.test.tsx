37eecac6fe99b94895a84b383d5e3967dbdecbbb
Zulfiqar Ali <01zulfiqarali@gmail.com>
2022-02-07 12:44:28 +0500

    Add tests for SignOutPage component

---
+++ src/tests/AuthComponents/SignOutPage.test.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import SignOutPage from '../../components/AuthComponents/SignOutPage';
+
+jest.mock(
+  '../../components/Loading',
+  () =>
+    function LoadingMock() {
+      return <h2>Loading component rendered</h2>;
+    },
+);
+
+const mockSignOutUser = jest.fn();
+
+jest.mock('../../utils/firebase', () => ({
+  signOutUser: () => mockSignOutUser(),
+}));
+
+describe('tests SignOutPage component', () => {
+  it('renders Loading component on initial render, then unmounts it', async () => {
+    render(<SignOutPage />);
+
+    await waitFor(() =>
+      expect(
+        screen.queryByText('Loading component rendered'),
+      ).not.toBeInTheDocument(),
+    );
+  });
+
+  it('renders successfully sign out heading', async () => {
+    render(<SignOutPage />);
+
+    const heading = await screen.findByRole('heading', {
+      name: 'successfully signed out',
+    });
+
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('invokes firebase.signOutUser method', () => {
+    render(<SignOutPage />);
+
+    expect(mockSignOutUser).toHaveBeenCalledTimes(1);
+  });
+});
