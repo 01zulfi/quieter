3c1e4f98590febe19d904645b099d236ec60b382
01zulfi <85733202+01zulfi@users.noreply.github.com>
2022-02-14 18:55:46 +0500

    Add isSubmitted state to ProfileEditForm component

--- src/components/Profile/ProfileEditForm.tsx
+++ src/components/Profile/ProfileEditForm.tsx
@@ -6,6 +6,7 @@ import Button from '../Button';
 const ProfileEditForm: FC = function ProfileEditForm() {
   const profile = useProfile() || { username: '' };
   const [username, setUsername] = useState(profile.username);
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
   const onUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) =>
     setUsername(event.target.value);
@@ -13,17 +14,29 @@ const ProfileEditForm: FC = function ProfileEditForm() {
   const onEditFormSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     await firebase.editUserDoc({ username });
+    setIsSubmitted(true);
   };
 
   return (
     <div>
-      <form onSubmit={onEditFormSubmit}>
-        <label htmlFor="username-edit">
-          edit username
-          <input type="text" onChange={onUsernameChange} value={username} />
-        </label>
-        <Button type="submit" textContent="Update" clickHandler={() => {}} />
-      </form>
+      {isSubmitted ? (
+        <div>
+          <h3>Profile updated</h3>
+        </div>
+      ) : (
+        <form onSubmit={onEditFormSubmit}>
+          <label htmlFor="username-edit">
+            edit username
+            <input
+              type="text"
+              id="username-edit"
+              onChange={onUsernameChange}
+              value={username}
+            />
+          </label>
+          <Button type="submit" textContent="Update" clickHandler={() => {}} />
+        </form>
+      )}
     </div>
   );
 };
