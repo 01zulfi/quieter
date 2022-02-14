08138523f272b800eb308e9950a47b08b683ce99
01zulfi <85733202+01zulfi@users.noreply.github.com>
2022-02-14 19:08:03 +0500

    Add ProfileEditableView component

---
+++ src/components/Profile/ProfileEditableView.tsx
@@ -0,0 +1,24 @@
+import React, { useState, FC } from 'react';
+import ProfileEditModal from './ProfileEditModal';
+import Button from '../Button';
+
+const ProfileEditableView: FC = function ProfileEditableView() {
+  const [showProfileEditModal, setShowProfileEditModal] = useState(false);
+
+  const onEditProfileClick = () => setShowProfileEditModal(true);
+  const onCloseModal = () => setShowProfileEditModal(false);
+
+  return (
+    <div>
+      <Button
+        type="button"
+        textContent="Edit profile"
+        clickHandler={onEditProfileClick}
+      />
+
+      {showProfileEditModal && <ProfileEditModal onCloseModal={onCloseModal} />}
+    </div>
+  );
+};
+
+export default ProfileEditableView;
