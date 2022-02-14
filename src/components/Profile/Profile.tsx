c276f1ac51aa1de3a8b431b8a8199e350b0241d6
01zulfi <85733202+01zulfi@users.noreply.github.com>
2022-02-14 18:11:12 +0500

    Add Profile component

---
+++ src/components/Profile/Profile.tsx
@@ -0,0 +1,47 @@
+import React, { FC, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { useUser } from '../../context/UserContext';
+import firebase from '../../utils/firebase';
+import Loading from '../Loading';
+import ProfileEditableView from './ProfileEditableView';
+import ProfileContext from '../../context/ProfileContext';
+
+const Profile: FC = function Profile() {
+  const params = useParams();
+  const [user, setUser] = useState<any>({});
+  const [isLoaded, setIsLoaded] = useState(false);
+  const signedInUser = useUser();
+
+  useEffect(() => {
+    if (params.userId === signedInUser.id) {
+      setIsLoaded(true);
+      return;
+    }
+
+    (async () => {
+      const fetchUser = await firebase.getNotSignedInUserDoc(
+        params.userId || '',
+      );
+      setUser(fetchUser);
+      setIsLoaded(true);
+    })();
+  }, []);
+
+  const isCurrentUserProfile = params.userId === signedInUser.id;
+
+  if (!isLoaded) return <Loading />;
+
+  return (
+    <div>
+      {isCurrentUserProfile && (
+        <ProfileContext.Provider value={user}>
+          <ProfileEditableView />
+        </ProfileContext.Provider>
+      )}
+
+      <h3>{user.username}</h3>
+    </div>
+  );
+};
+
+export default Profile;
