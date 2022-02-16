63cfd9773c923a1646be6d70aa289812eeb2210c
01zulfi <85733202+01zulfi@users.noreply.github.com>
2022-02-16 17:11:03 +0500

    Add button to toggle BoxCreateModal in Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,11 +1,25 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
+import BoxCreateModal from './Boxes/BoxCreateModal';
+import Button from './Button';
 
 const Sidebar: FC = function Sidebar() {
+  const [showBoxCreateModal, setShowBoxCreateModal] = useState(false);
+
+  const onCreateBoxClick = () => setShowBoxCreateModal(true);
+  const onCloseModal = () => setShowBoxCreateModal(false);
+
   return (
     <section>
       <div>
         <h3>Explore Boxes</h3>
+        <Button
+          type="button"
+          textContent="Create box"
+          clickHandler={onCreateBoxClick}
+        />
       </div>
+
+      {showBoxCreateModal && <BoxCreateModal closeModal={onCloseModal} />}
     </section>
   );
 };
