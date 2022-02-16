278aaff70ad9ab51ba4f973e1549d17f4a25b1cc
01zulfi <85733202+01zulfi@users.noreply.github.com>
2022-02-16 17:01:19 +0500

    Rewrite tests for Feed component

--- src/tests/Feed.test.tsx
+++ src/tests/Feed.test.tsx
@@ -1,36 +1,74 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Feed from '../components/Feed';
 
-jest.mock('../context/UserContext', () => ({
-  useUser: () => ({
-    associatedStrings: ['1e1', '22'],
-  }),
+const mockGetFeedStrings = jest.fn();
+
+jest.mock('../utils/firebase', () => ({
+  getFeedStrings: async () => mockGetFeedStrings(),
 }));
 
 jest.mock(
   '../components/Strings/StringCompactView',
   () =>
-    function StringCompactView({ stringId }: { stringId: string }) {
+    function StringCompactViewMock({ stringId }: { stringId: string }) {
       return <h2>{stringId}</h2>;
     },
 );
 
-describe('tests Feed component', () => {
-  test('renders same number of StringCompactView as associated strings', () => {
-    const { getAllByRole } = render(<Feed />);
-    const headings = getAllByRole('heading');
+jest.mock(
+  '../components/Loading',
+  () =>
+    function LoadingMock() {
+      return <h2>Loading component rendered</h2>;
+    },
+);
 
-    expect(headings).toHaveLength(2);
+describe('tests Feed component', () => {
+  it('renders Loading component upon first page load, and then disappears', async () => {
+    render(<Feed />);
+    await waitFor(() =>
+      expect(
+        screen.queryByText('Loading component rendered'),
+      ).not.toBeInTheDocument(),
+    );
   });
 
-  test('passes correct props to StringCompactView', () => {
-    const { getAllByRole } = render(<Feed />);
-    const headingOne = getAllByRole('heading')[0];
-    const headingTwo = getAllByRole('heading')[1];
+  it('renders no feed available message when feedStrings is null/undefined', async () => {
+    mockGetFeedStrings.mockImplementation(() => null);
+    render(<Feed />);
 
-    expect(headingOne).toHaveTextContent('1e1');
-    expect(headingTwo).toHaveTextContent('22');
+    const heading = await screen.findByRole('heading', {
+      name: 'No feed available. Populate it by getting involved!',
+    });
+
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('renders no feed available message when feedStrings is an empty array', async () => {
+    mockGetFeedStrings.mockImplementation(() => []);
+    render(<Feed />);
+
+    const heading = await screen.findByRole('heading', {
+      name: 'No feed available. Populate it by getting involved!',
+    });
+
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('renders StringCompactView component according to feedStrings data', async () => {
+    mockGetFeedStrings.mockImplementation(() => ['id1', 'str']);
+    render(<Feed />);
+
+    const stringOne = await screen.findByRole('heading', {
+      name: 'id1',
+    });
+    const stringTwo = await screen.findByRole('heading', {
+      name: 'str',
+    });
+
+    expect(stringOne).toBeInTheDocument();
+    expect(stringTwo).toBeInTheDocument();
   });
 });
