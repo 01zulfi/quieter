e8c8a6fd1ea087b5ab883d2969fcb5181e1f0431
01zulfi <85733202+01zulfi@users.noreply.github.com>
2022-02-18 17:15:29 +0500

    Add reset.css & normalize.css

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import RouteSwitch from './RouteSwitch';
+import './styles/reset.css';
+import './styles/normalize.css';
 
 ReactDOM.render(
   <React.StrictMode>
