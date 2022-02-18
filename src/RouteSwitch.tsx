9e46292280debc28ca03ac8b81881b66c55a0285
01zulfi <85733202+01zulfi@users.noreply.github.com>
2022-02-18 17:04:02 +0500

    Add AppWrapper in RouteSwitch component

--- src/RouteSwitch.tsx
+++ src/RouteSwitch.tsx
@@ -1,11 +1,16 @@
 import React, { FC, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
 import SignInPage from './components/AuthComponents/SignInPage';
 import SignOutPage from './components/AuthComponents/SignOutPage';
 import SetThemeContext from './context/SetThemeContext';
 import theme from './utils/themes';
 import App from './App';
+
+const AppWrapper = styled.section`
+  background: ${(props: any) => props.theme.base.one};
+  color: ${(props: any) => props.theme.text.one};
+`;
 
 const RouteSwitch: FC = function RouteSwitch() {
   const [currentTheme, setCurrentTheme] = useState<any>(theme.light);
@@ -13,13 +18,15 @@ const RouteSwitch: FC = function RouteSwitch() {
   return (
     <ThemeProvider theme={currentTheme}>
       <SetThemeContext.Provider value={setCurrentTheme}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/sign-in" element={<SignInPage />} />
-            <Route path="/sign-out" element={<SignOutPage />} />
-            <Route path="/*" element={<App />} />
-          </Routes>
-        </BrowserRouter>
+        <AppWrapper>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/sign-in" element={<SignInPage />} />
+              <Route path="/sign-out" element={<SignOutPage />} />
+              <Route path="/*" element={<App />} />
+            </Routes>
+          </BrowserRouter>
+        </AppWrapper>
       </SetThemeContext.Provider>
     </ThemeProvider>
   );
