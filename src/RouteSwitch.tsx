892e8e0f02b18f12093fb92b037f679420a31b3b
01zulfi <85733202+01zulfi@users.noreply.github.com>
2022-02-18 13:57:04 +0500

    Add currentTheme state in RouteSwitch.tsx - Use ThemeProvider to provide theme to children - Use SetThemeContext to allow children to change theme

--- src/RouteSwitch.tsx
+++ src/RouteSwitch.tsx
@@ -1,18 +1,27 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 import SignInPage from './components/AuthComponents/SignInPage';
 import SignOutPage from './components/AuthComponents/SignOutPage';
+import SetThemeContext from './context/SetThemeContext';
+import theme from './utils/themes';
 import App from './App';
 
 const RouteSwitch: FC = function RouteSwitch() {
+  const [currentTheme, setCurrentTheme] = useState<any>(theme.light);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/sign-in" element={<SignInPage />} />
-        <Route path="/sign-out" element={<SignOutPage />} />
-        <Route path="/*" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={currentTheme}>
+      <SetThemeContext.Provider value={setCurrentTheme}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/sign-in" element={<SignInPage />} />
+            <Route path="/sign-out" element={<SignOutPage />} />
+            <Route path="/*" element={<App />} />
+          </Routes>
+        </BrowserRouter>
+      </SetThemeContext.Provider>
+    </ThemeProvider>
   );
 };
 
