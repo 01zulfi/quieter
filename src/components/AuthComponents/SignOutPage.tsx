4b8de508abec92b810bb9e6f33c86f944a400c7f
01zulfi <85733202+01zulfi@users.noreply.github.com>
2022-02-19 13:43:11 +0500

    Style SignOutPage.tsx

--- src/components/AuthComponents/SignOutPage.tsx
+++ src/components/AuthComponents/SignOutPage.tsx
@@ -1,6 +1,32 @@
 import React, { FC, useState, useEffect } from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 import Loading from '../Loading';
+import StrippedNavbar from '../Navbar/StrippedNavbar';
 import firebase from '../../utils/firebase';
+
+const SignOutPageWrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 5em;
+  min-height: inherit;
+  background: ${(props: any) => props.theme.base.one};
+`;
+
+const MessageWrapper = styled.section`
+  background: ${(props: any) => props.theme.base.two};
+  color: ${(props: any) => props.theme.text.one};
+  align-self: center;
+  width: 75%;
+  height: 10em;
+  justify-content: space-around;
+  border: 1px solid ${(props: any) => props.theme.frost.one};
+  border-radius: 1em;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  box-shadow: rgb(36 41 51 / 15%) 0px 5px 10px 0px;
+`;
 
 const SignOutPage: FC = function SignOutPage() {
   const [isSignedOut, setIsSignedOut] = useState(false);
@@ -11,9 +37,17 @@ const SignOutPage: FC = function SignOutPage() {
   }, []);
 
   return (
-    <section>
-      {isSignedOut ? <h2>successfully signed out</h2> : <Loading />}
-    </section>
+    <SignOutPageWrapper>
+      <StrippedNavbar />
+      {isSignedOut ? (
+        <MessageWrapper>
+          <h3>Successfully signed out of quieter</h3>
+          <Link to="/sign-in">Sign in</Link>
+        </MessageWrapper>
+      ) : (
+        <Loading />
+      )}
+    </SignOutPageWrapper>
   );
 };
 
