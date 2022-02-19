4a0414d933c4df99df29a06641a987746fa3e57b
01zulfi <85733202+01zulfi@users.noreply.github.com>
2022-02-19 14:41:13 +0500

    Style SignInPrompt.tsx

--- src/components/AuthComponents/SignInPrompt.tsx
+++ src/components/AuthComponents/SignInPrompt.tsx
@@ -1,12 +1,43 @@
 import React, { FC } from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import StyledLink from '../StyledLink';
+import StrippedNavbar from '../Navbar/StrippedNavbar';
+
+const SignInPromptWrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 5em;
+  min-height: inherit;
+  background: ${(props: any) => props.theme.base.one};
+`;
+
+const MessageWrapper = styled.section`
+  background: ${(props: any) => props.theme.base.two};
+  color: ${(props: any) => props.theme.text.one};
+  align-self: center;
+  width: 75%;
+  height: 10em;
+  justify-content: space-around;
+  border: 1px solid ${(props: any) => props.theme.frost.one};
+  border-radius: 1em;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  box-shadow: rgb(36 41 51 / 15%) 0px 5px 10px 0px;
+`;
 
 const SignInPrompt: FC = function SignInPrompt() {
   return (
-    <div>
-      not signed in
-      <Link to="sign-in">sign in here</Link>
-    </div>
+    <SignInPromptWrapper>
+      <StrippedNavbar />
+      <MessageWrapper>
+        <h3>Looks like you&apos;re not signed in...</h3>
+        <StyledLink size="2em" bold="800">
+          <Link to="sign-in">sign in here</Link>
+        </StyledLink>
+      </MessageWrapper>
+    </SignInPromptWrapper>
   );
 };
 
