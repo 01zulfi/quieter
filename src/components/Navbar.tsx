2f1b9158f10c9f22711dbd8be4ffb6db397754ff
01zulfi <85733202+01zulfi@users.noreply.github.com>
2022-02-19 12:49:30 +0500

    Make Navbar responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,20 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { Link } from 'react-router-dom';
 import styled, { useTheme } from 'styled-components';
 import { useSetTheme } from '../context/SetThemeContext';
 import theme from '../utils/themes';
-import Button from './Button';
+import menuLight from '../icons/menu_white_24dp.svg';
+import menuDark from '../icons/menu_black_24dp.svg';
+import cancelLight from '../icons/cancel_white_24dp.svg';
+import cancelDark from '../icons/cancel_black_24dp.svg';
+import lightTheme from '../icons/light_mode_white_24dp.svg';
+import darkTheme from '../icons/dark_mode_black_24dp.svg';
 
 const NavbarWrapper = styled.div`
   display: flex;
   justify-content: space-between;
   box-shadow: rgb(36 41 51 / 15%) 0px 5px 10px 0px;
-  background: ${(props: any) => props.theme.base.one};
+  background: ${(props: any) => props.background};
   height: 5rem;
 `;
 
@@ -31,8 +36,23 @@ const H2Wrapper = styled.h2`
 `;
 
 const NavWrapper = styled.nav`
-  display: flex;
+  display: ${(props: any) => (props.shouldShow ? 'flex' : 'none')};
   align-items: center;
+
+  @media only screen and (max-width: 680px) {
+    background: rgba(255, 255, 255, 0.01);
+    font-weight: bold;
+    font-size: 1.4em;
+    position: absolute;
+    right: 0;
+    height: 100vh;
+    width: 80vw;
+    justify-content: center;
+    align-items: flex-start;
+    padding-top: 5em;
+    gap: 3em;
+    backdrop-filter: blur(1rem);
+  }
 `;
 
 const List = styled.ul`
@@ -43,37 +63,83 @@ const List = styled.ul`
   list-style: none;
   align-items: center;
   margin-right: 1em;
+
+  flex-direction: column;
+
+  @media only screen and (min-width: 680px) {
+    display: flex;
+    flex-direction: row;
+  }
+`;
+
+const Icon = styled.button`
+  background: transparent;
+  border: 0;
+  position: fixed;
+  right: 2em;
+  top: 1em;
+  z-index: 999;
+
+  @media only screen and (min-width: 680px) {
+    display: none;
+  }
+`;
+
+const Icon2 = styled.button`
+  background: transparent;
+  border: 0;
 `;
 
 const Navbar: FC = function Navbar() {
-  const currentTheme = useTheme().name;
+  const currentTheme = useTheme();
   const setCurrentTheme = useSetTheme();
+  const [showMenu, setShowMenu] = useState(screen.width > 680);
 
   const switchTheme = () => {
-    if (currentTheme === 'light') {
+    if (currentTheme.name === 'light') {
       setCurrentTheme(theme.dark);
       return;
     }
     setCurrentTheme(theme.light);
   };
 
+  const onMenuToggle = () => setShowMenu((prev) => !prev);
+
+  const iconDecider = () => {
+    if (showMenu) {
+      if (currentTheme.name === 'light') return cancelDark;
+      return cancelLight;
+    }
+    return currentTheme.name === 'light' ? menuDark : menuLight;
+  };
+
   return (
-    <NavbarWrapper>
+    <NavbarWrapper
+      background={
+        currentTheme.name === 'light' ? 'white' : currentTheme.base.one
+      }
+    >
       <HeadingWrapper>
         <H1Wrapper>Q</H1Wrapper>
         <H2Wrapper>uieter</H2Wrapper>
       </HeadingWrapper>
-      <NavWrapper>
+
+      <Icon onClick={onMenuToggle}>
+        <img src={iconDecider()} alt="hamburger" style={{ width: '3em' }} />
+      </Icon>
+
+      <NavWrapper shouldShow={showMenu}>
         <List>
           <li>About</li>
-          <li>
-            <Button
-              textContent={currentTheme === 'light' ? 'dark' : 'light'}
-              type="button"
-              clickHandler={switchTheme}
-            />
-          </li>
           <li>Profile</li>
+          <li>
+            <Icon2 onClick={switchTheme}>
+              <img
+                src={currentTheme.name === 'light' ? darkTheme : lightTheme}
+                alt="theme"
+              />
+            </Icon2>
+          </li>
           <li>
             <Link to="/sign-out">Sign out</Link>
           </li>
