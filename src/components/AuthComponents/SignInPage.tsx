beed132c4655d889b309e511fa136d4ee64c5527
01zulfi <85733202+01zulfi@users.noreply.github.com>
2022-02-19 19:21:42 +0500

    Update SignInPage component - Add theme switcher icons - Show StrippedNavbar on smaller screens instead of image

--- src/components/AuthComponents/SignInPage.tsx
+++ src/components/AuthComponents/SignInPage.tsx
@@ -1,16 +1,22 @@
 import React, { FC } from 'react';
-import styled from 'styled-components';
+import styled, { useTheme } from 'styled-components';
 import { Link } from 'react-router-dom';
 import SignInForm from './SignInForm';
 import StyledLink from '../StyledLink';
+import { useSetTheme } from '../../context/SetThemeContext';
 import image from '../../images/johanna-buguet-5d5H42WDT4M-unsplash.jpg';
+import theme from '../../utils/themes';
+import lightTheme from '../../icons/light_mode_white_24dp.svg';
+import darkTheme from '../../icons/dark_mode_black_24dp.svg';
+import StrippedNavbar from '../Navbar/StrippedNavbar';
 
 const SignInPageWrapper = styled.section`
   display: flex;
   justify-center: flex;
   min-height: inherit;
 
-  @media (min-width: 680px) {
+  @media (max-width: 680px) {
+    flex-direction: column;
   }
 `;
 
@@ -66,11 +72,54 @@ const LinkWrapper = styled.div`
   align-items: center;
 `;
 
+const Wrapper = styled.section``;
+
+const Icon = styled.button`
+  background: transparent;
+  border: 0;
+  margin-right: 1em;
+  align-self: center;
+  padding: 5px;
+  border-radius: 2px;
+  position: fixed;
+  right: 2em;
+  top: 1em;
+
+  &:hover {
+    background: ${(props: any) => props.theme.base.four};
+  }
+
+  @media (max-width: 680px) {
+    display: none;
+  }
+`;
+
+const NavbarResponsiveWrapper = styled.div`
+  display: none;
+  @media (max-width: 680px) {
+    display: block;
+  }
+`;
+
 const SignInPage: FC = function SignInPage() {
+  const currentTheme = useTheme();
+  const setCurrentTheme = useSetTheme();
   const isUserSignedIn = localStorage.getItem('isSignedIn') === 'true';
+
+  const switchTheme = () => {
+    if (currentTheme.name === 'light') {
+      setCurrentTheme(theme.dark);
+      return;
+    }
+    setCurrentTheme(theme.light);
+  };
 
   return (
     <SignInPageWrapper>
+      <NavbarResponsiveWrapper>
+        <StrippedNavbar />
+      </NavbarResponsiveWrapper>
+
       <ImageContainer>
         <HeadingWrapper>
           <H1Wrapper>Q</H1Wrapper>
@@ -99,16 +148,25 @@ const SignInPage: FC = function SignInPage() {
         </Credits>
       </ImageContainer>
 
-      {isUserSignedIn ? (
-        <div>
-          <h2>You&apos;re already signed in!</h2>
-          <StyledLink size="1.5em" bold="500">
-            <Link to="/home">Go to homepage</Link>
-          </StyledLink>
-        </div>
-      ) : (
-        <SignInForm />
-      )}
+      <Wrapper>
+        <Icon onClick={switchTheme}>
+          <img
+            src={currentTheme.name === 'light' ? darkTheme : lightTheme}
+            alt="theme"
+          />
+        </Icon>
+
+        {isUserSignedIn ? (
+          <div>
+            <h2>You&apos;re already signed in!</h2>
+            <StyledLink size="1.5em" bold="500">
+              <Link to="/home">Go to homepage</Link>
+            </StyledLink>
+          </div>
+        ) : (
+          <SignInForm />
+        )}
+      </Wrapper>
     </SignInPageWrapper>
   );
 };
