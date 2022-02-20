bf758636faca478713730fa9fcd5b170fd6eb383
01zulfi <85733202+01zulfi@users.noreply.github.com>
2022-02-20 11:29:36 +0500

    Style Modal.tsx

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,18 +1,42 @@
 import React, { FC } from 'react';
+import styled from 'styled-components';
 import Button from './Button';
 
 interface ModalProps {
   closeModal: React.MouseEventHandler<HTMLButtonElement>;
 }
 
+const ModalWrapper = styled.section`
+  width: 100vw;
+  height: 100vh;
+  position: absolute;
+  left: 0;
+  top: 0;
+  background: rgba(255, 255, 255, 0.1);
+  backdrop-filter: blur(1rem);
+  display: flex;
+  justify-content: center;
+`;
+
+const ContentWrapper = styled.section`
+  margin-top: 4rem;
+`;
+
 const Modal: FC<ModalProps> = function Modal({ closeModal, children }) {
   return (
-    <section>
-      <div>{children}</div>
-      <div>
-        <Button type="button" textContent="Close" clickHandler={closeModal} />
-      </div>
-    </section>
+    <ModalWrapper>
+      <ContentWrapper>
+        <div>{children}</div>
+        <div>
+          <Button
+            type="button"
+            textContent="Close"
+            clickHandler={closeModal}
+            status="red"
+          />
+        </div>
+      </ContentWrapper>
+    </ModalWrapper>
   );
 };
 
