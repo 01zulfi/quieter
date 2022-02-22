5c2b789647a5e77d4dfee1146d1f5716e0c154d1
01zulfi <85733202+01zulfi@users.noreply.github.com>
2022-02-22 20:11:25 +0500

    Start styling StringContainer

--- src/components/Strings/StringContainer.tsx
+++ src/components/Strings/StringContainer.tsx
@@ -1,5 +1,6 @@
 import React, { FC, useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
+import styled from 'styled-components';
 import { useUser, useUserAnon } from '../../context/UserContext';
 import StringContext from '../../context/StringContext';
 import StringAuthorView from './StringAuthorView';
@@ -9,6 +10,31 @@ import KnotCreate from '../Knots/KnotCreate';
 import Loading from '../Loading';
 import firebase from '../../utils/firebase';
 import StyledLink from '../StyledLink';
+
+const StringWrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  border-radius: 10px;
+  box-shadow: rgb(36 41 51 / 15%) 0px 5px 10px 0px;
+  background: ${(props: any) => props.theme.base.two};
+`;
+
+const MetaInfoWrapper = styled.div`
+  display: flex;
+  flex-direction: row-reverse;
+  justify-content: space-between;
+  background: ${(props: any) => props.theme.base.three};
+  width: 100%;
+  color: ${(props: any) => props.theme.text.two};
+  font-size: 0.9em;
+  opacity: 0.9;
+  padding: 0 1em;
+
+  div {
+    flex-grow: 1;
+    max-width: fit-content;
+  }
+`;
 
 const StringContainer: FC = function StringContainer() {
   const params = useParams();
@@ -35,16 +61,26 @@ const StringContainer: FC = function StringContainer() {
   }
 
   return (
-    <section>
+    <StringWrapper>
       <StringContext.Provider value={string}>
-        <div>
-          <p>posted in</p>
-          <StyledLink size="1em" bold="normal">
-            <Link to={`../../../../box/${string.associatedBox.id}`} replace>
-              {string.associatedBox.name}
-            </Link>
-          </StyledLink>
-        </div>
+        <MetaInfoWrapper>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <p>posted in</p>
+            <StyledLink size="1em" bold="normal" black>
+              <Link to={`../../../../box/${string.associatedBox.id}`} replace>
+                {string.associatedBox.name}
+              </Link>
+            </StyledLink>
+          </div>
+
+          <div>
+            <StyledLink size="1em" bold="normal" black>
+              <Link to={`../../../../profile/${string.author.id}`} replace>
+                {string.author.username}
+              </Link>
+            </StyledLink>
+          </div>
+        </MetaInfoWrapper>
 
         {isCurrentUserAuthor && <StringAuthorView />}
 
@@ -61,7 +97,7 @@ const StringContainer: FC = function StringContainer() {
           {!isUserAnon && <KnotCreate />}
         </div>
       </StringContext.Provider>
-    </section>
+    </StringWrapper>
   );
 };
 
