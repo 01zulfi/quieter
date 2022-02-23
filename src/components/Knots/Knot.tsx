04350839c275928d896b9e560bf2be90b0901834
01zulfi <85733202+01zulfi@users.noreply.github.com>
2022-02-23 17:53:48 +0500

    Update Knot.tsx - Style component - Add isLoading state - Add condition if knot is null - Add delete button

--- src/components/Knots/Knot.tsx
+++ src/components/Knots/Knot.tsx
@@ -1,6 +1,33 @@
 import React, { FC, useState, useEffect } from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 import firebase from '../../utils/firebase';
 import { useUser, useUserAnon } from '../../context/UserContext';
+import StyledLink from '../StyledLink';
+import Loading from '../Loading';
+import Button from '../Button';
+
+const KnotWrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  border-radius: 5px;
+  background: ${(props: any) => props.theme.base.three};
+  outline: 1px solid ${(props: any) => props.theme.frost.one};
+  flex-grow: 1;
+`;
+
+const AuthorWrapper = styled.div`
+  background: ${(props: any) => props.theme.base.four};
+  font-size: 0.9em;
+  padding: 0.1em 0.3em;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const ContentWrapper = styled.div`
+  flex-grow: 1;
+  padding: 0.3em 0.3em;
+`;
 
 interface KnotProps {
   knotId: string;
@@ -8,6 +35,7 @@ interface KnotProps {
 
 const Knot: FC<KnotProps> = function Knot({ knotId }) {
   const [knot, setKnot] = useState<any>({});
+  const [isLoaded, setIsLoaded] = useState(false);
   const user = useUser();
   const isUserAnon = useUserAnon();
 
@@ -15,20 +43,43 @@ const Knot: FC<KnotProps> = function Knot({ knotId }) {
     (async () => {
       const fetchKnot = await firebase.getKnot(knotId);
       setKnot(fetchKnot);
+      setIsLoaded(true);
     })();
   }, []);
+
+  if (!isLoaded) return <Loading />;
+  if (!knot) return <h4>Unable to load</h4>;
 
   const isCurrentUserAuthor =
     !isUserAnon && user.authoredKnots.some((id: string) => knotId === id);
 
-  // TODO: show user name & avatar
+  const deleteHandler = async () => {
+    await firebase.deleteKnot({ knotId, stringId: knot.associatedString });
+  };
+
   return (
-    <div>
-      {isCurrentUserAuthor && <p>you are the author of this knot</p>}
-      <div>
+    <KnotWrapper>
+      <AuthorWrapper>
+        <StyledLink size="1em" bold="normal" black>
+          <Link to={`../../../../profile/${knot.author.id}`}>
+            {knot.author.username}
+          </Link>
+        </StyledLink>
+        {isCurrentUserAuthor && (
+          <Button
+            textContent="Delete"
+            status="red"
+            padding="0.4em"
+            clickHandler={deleteHandler}
+            type="button"
+          />
+        )}
+      </AuthorWrapper>
+
+      <ContentWrapper>
         <p>{knot.content}</p>
-      </div>
-    </div>
+      </ContentWrapper>
+    </KnotWrapper>
   );
 };
 
