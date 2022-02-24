ca972c4466f542607a7e6147fe6fc16ed001d4b2
01zulfi <85733202+01zulfi@users.noreply.github.com>
2022-02-24 18:12:23 +0500

    Refactor & style StringDelete.tsx

--- src/components/Strings/StringDelete.tsx
+++ src/components/Strings/StringDelete.tsx
@@ -1,35 +1,53 @@
-import React, { FC, useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { FC } from 'react';
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
 import firebase from '../../utils/firebase';
 import { useString } from '../../context/StringContext';
 import Button from '../Button';
 
+const StringDeleteWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1em;
+  padding: 1em;
+  margin: 1em;
+  border-radius: 10px;
+  box-shadow: rgb(36 41 51 / 15%) 0px 5px 10px 0px;
+  background: ${(props: any) => props.theme.base.three};
+`;
+
 const StringDelete: FC = function StringDelete() {
-  const string = useString() || { id: 'DEFAULT', associatedBox: 'DEFAULT' };
-  const [isDeleted, setIsDeleted] = useState(false);
+  const string = useString() || {
+    id: 'DEFAULT',
+    associatedBox: { id: 'DEFAULT' },
+  };
+  const navigate = useNavigate();
 
   const onBoxDelete = async () => {
     await firebase.deleteString(string.id);
-    setIsDeleted(true);
+    navigate(`../../../../box/${string.associatedBox.id}`);
   };
 
+  if (string.id === 'DEFAULT') {
+    return <h4>Unable to load, try refreshing.</h4>;
+  }
+
   return (
-    <div>
-      {isDeleted ? (
-        <div>
-          <h2>the string has been deleted</h2>
-          <Link to={`/box/${string.associatedBox}`}>go back to box</Link>
-        </div>
-      ) : (
-        <div>
-          <Button
-            type="submit"
-            textContent="Delete String"
-            clickHandler={onBoxDelete}
-          />
-        </div>
-      )}
-    </div>
+    <StringDeleteWrapper>
+      <h3>Are you sure?</h3>
+      <p>
+        <strong>
+          Deleting this string will also delete all the knots that were tied to
+          it.
+        </strong>
+      </p>
+      <Button
+        type="submit"
+        status="red"
+        textContent="Delete String"
+        clickHandler={onBoxDelete}
+      />
+    </StringDeleteWrapper>
   );
 };
 
