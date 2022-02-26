e67f20a865583c5751c81a6cb4559ad6f16f43ad
01zulfi <85733202+01zulfi@users.noreply.github.com>
2022-02-26 17:32:37 +0500

    Refactor BoxRegularView - Style component - Add join & leave box logic - Add isButtonLoaded state for smooth transition for leaving and joining

--- src/components/Boxes/BoxRegularView.tsx
+++ src/components/Boxes/BoxRegularView.tsx
@@ -1,26 +1,117 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
+import styled from 'styled-components';
 import Button from '../Button';
 import { useBox } from '../../context/BoxContext';
+import firebase from '../../utils/firebase';
+import { useUserAnon } from '../../context/UserContext';
+import Loading from '../Loading';
+
+const BoxRegularViewWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.8em;
+`;
+
+const BoxNameWrapper = styled.h2`
+  text-align: start;
+  margin-top: 0.5em;
+`;
+
+const DescAndButtonWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  p {
+    background: ${(props: any) => props.theme.base.three};
+    padding: 0.3em;
+  }
+`;
+
+const Line = styled.div`
+  background: ${(props: any) => props.theme.base.four};
+  width: 100%;
+  height: 0.3em;
+  margin: 1em 0em;
+`;
 
 interface BoxRegularViewProps {
   onButtonClick: React.MouseEventHandler<HTMLButtonElement>;
+  isCurrentUserAdmin: boolean;
+  isCurrentUserMember: boolean;
 }
 
 const BoxRegularView: FC<BoxRegularViewProps> = function BoxRegularView({
   onButtonClick,
+  isCurrentUserAdmin,
+  isCurrentUserMember,
 }) {
-  const box = useBox() || { name: '', description: '' };
+  const [isButtonLoaded, setIsButtonLoaded] = useState(true);
+  const box = useBox() || { id: 'DEFAULT', name: '', description: '' };
+  const isUserAnon = useUserAnon();
+
+  if (box.id === 'DEFAULT') {
+    return <h4>Unable to load, please try refreshing.</h4>;
+  }
+
+  const onJoinButtonClick = async () => {
+    setIsButtonLoaded(false);
+    await firebase.joinBox(box.id);
+    setIsButtonLoaded(true);
+  };
+
+  const onLeaveButtonClick = async () => {
+    setIsButtonLoaded(false);
+    await firebase.leaveBox(box.id);
+    setIsButtonLoaded(true);
+  };
 
   return (
-    <div>
-      <h2>{box.name}</h2>
-      <p>{box.description}</p>
+    <BoxRegularViewWrapper>
+      <BoxNameWrapper>{box.name}</BoxNameWrapper>
+
+      <DescAndButtonWrapper>
+        <p>{box.description}</p>
+
+        {isButtonLoaded ? (
+          <div>
+            {!isCurrentUserAdmin && (
+              <div>
+                {isCurrentUserMember ? (
+                  <Button
+                    type="button"
+                    textContent="Leave Box"
+                    clickHandler={onLeaveButtonClick}
+                    status="orange"
+                    padding="0.9em"
+                  />
+                ) : (
+                  <Button
+                    textContent="Join Box"
+                    type="button"
+                    clickHandler={isUserAnon ? () => {} : onJoinButtonClick}
+                    status="purple"
+                    padding="0.9em"
+                  />
+                )}
+              </div>
+            )}
+          </div>
+        ) : (
+          <Loading width="2em" />
+        )}
+      </DescAndButtonWrapper>
+
+      <Line />
+
       <Button
+        status="secondary"
+        padding="1em"
         type="button"
         textContent={`Start a string in ${box.name}`}
-        clickHandler={onButtonClick}
+        clickHandler={isUserAnon ? () => {} : onButtonClick}
       />
-    </div>
+    </BoxRegularViewWrapper>
   );
 };
 
