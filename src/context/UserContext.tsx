1b05ae6c38dbac2dd6fcb33ad48d2bf0ad9031af
01zulfi <85733202+01zulfi@users.noreply.github.com>
2022-02-26 14:54:27 +0500

    Refactor UserContext to use firebase.listenForUserChanges instead of relying on SetUserContext

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -4,15 +4,11 @@ import React, {
   useContext,
   useState,
   useEffect,
-  useCallback,
 } from 'react';
 import UserInterface from '../interfaces/UserInterface';
 import firebase from '../utils/firebase';
 
-const emptyFunction = () => {};
-
 const UserContext = createContext<any>(null);
-const SetUserContext = createContext<any>(emptyFunction);
 const UserAnonymousContext = createContext(false);
 
 const UserProvider: FC = function UserProvider({ children }) {
@@ -24,36 +20,29 @@ const UserProvider: FC = function UserProvider({ children }) {
     (async () => {
       if (isUserAnon) {
         setIsLoaded(true);
+        return;
       }
       const fetchedUserData: UserInterface = await firebase.getUserDoc();
       setUser(fetchedUserData);
       setIsLoaded(true);
     })();
-  }, []);
 
-  const userContextUpdater = useCallback(() => {
-    (async () => {
-      const fetchedUserData: UserInterface = await firebase.getUserDoc();
-      setUser(fetchedUserData);
-    })();
+    const listener = firebase.listenForUserChanges();
+    const unsubscribe = listener(setUser);
+    return unsubscribe();
   }, []);
 
   return (
     <UserContext.Provider value={isUserAnon ? null : user}>
-      <SetUserContext.Provider
-        value={isUserAnon ? emptyFunction : userContextUpdater}
-      >
-        <UserAnonymousContext.Provider value={isUserAnon}>
-          {isLoaded && children}
-        </UserAnonymousContext.Provider>
-      </SetUserContext.Provider>
+      <UserAnonymousContext.Provider value={isUserAnon}>
+        {isLoaded && children}
+      </UserAnonymousContext.Provider>
     </UserContext.Provider>
   );
 };
 
 const useUser = () => useContext(UserContext);
-const useSetUser = () => useContext(SetUserContext);
 const useUserAnon = () => useContext(UserAnonymousContext);
 
 export default UserProvider;
-export { useUser, useSetUser, useUserAnon };
+export { useUser, useUserAnon };
