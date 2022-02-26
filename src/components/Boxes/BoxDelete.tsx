86a4a20874c62657f92630333b40319612e1838b
01zulfi <85733202+01zulfi@users.noreply.github.com>
2022-02-26 19:19:24 +0500

    Update BoxDelete.tsx - Style component - Replace Link with useNavigate

--- src/components/Boxes/BoxDelete.tsx
+++ src/components/Boxes/BoxDelete.tsx
@@ -1,35 +1,50 @@
-import React, { FC, useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { FC } from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
 import { useBox } from '../../context/BoxContext';
 import firebase from '../../utils/firebase';
 import Button from '../Button';
 
+const BoxDeleteWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1em;
+  padding: 1em;
+  margin: 1em;
+  border-radius: 10px;
+  box-shadow: rgb(36 41 51 / 15%) 0px 5px 10px 0px;
+  background: ${(props: any) => props.theme.base.three};
+`;
+
 const BoxDelete: FC = function BoxDelete() {
   const box = useBox() || { id: 'DEFAULT' };
-  const [isDeleted, setIsDeleted] = useState(false);
+  const navigate = useNavigate();
+
+  if (box.id === 'DEFAULT') {
+    return <h4>Unable to load, try refreshing.</h4>;
+  }
 
   const onBoxDelete = async () => {
     await firebase.deleteBox(box.id);
-    setIsDeleted(true);
+    navigate('../../../../');
   };
 
   return (
-    <div>
-      {isDeleted ? (
-        <div>
-          <h2>the box has been deleted</h2>
-          <Link to="/">go back to home</Link>
-        </div>
-      ) : (
-        <div>
-          <Button
-            type="submit"
-            textContent="Delete box"
-            clickHandler={onBoxDelete}
-          />
-        </div>
-      )}
-    </div>
+    <BoxDeleteWrapper>
+      <h2>Are you sure?</h2>
+      <p>
+        <strong>
+          Deleting this box will delete all the strings and knots associated
+          with it.
+        </strong>
+      </p>
+      <Button
+        type="submit"
+        textContent="Delete box"
+        status="red"
+        clickHandler={onBoxDelete}
+      />
+    </BoxDeleteWrapper>
   );
 };
 
