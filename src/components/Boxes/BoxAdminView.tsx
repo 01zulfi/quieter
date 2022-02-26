dd985472d0f4281756fe5cdbdaba7955c3d8d05d
01zulfi <85733202+01zulfi@users.noreply.github.com>
2022-02-26 16:36:38 +0500

    Style BoxAdminView.tsx

--- src/components/Boxes/BoxAdminView.tsx
+++ src/components/Boxes/BoxAdminView.tsx
@@ -1,8 +1,23 @@
 import React, { FC, useState } from 'react';
+import styled from 'styled-components';
 import Modal from '../Modal';
 import Button from '../Button';
 import BoxEdit from './BoxEdit';
 import BoxDelete from './BoxDelete';
+
+const BoxAdminViewWrapper = styled.div`
+  display: flex;
+  width: 100%;
+`;
+
+const ButtonsWrapper = styled.div`
+  flex-grow: 1;
+  display: flex;
+  padding: 0.4em 0.8em;
+  border-radius: 5px;
+  gap: 0.5em;
+  background: ${(props: any) => props.theme.aurora.five};
+`;
 
 const BoxAdminView: FC = function BoxAdminView() {
   const [showEditModal, setShowEditModal] = useState(false);
@@ -15,33 +30,36 @@ const BoxAdminView: FC = function BoxAdminView() {
   const onCloseDeleteModal = (): void => setShowDeleteModal(false);
 
   return (
-    <div>
-      {!showEditModal && !showDeleteModal && (
-        <div>
-          <p>you are the admin of this box</p>
-          <Button
-            type="button"
-            textContent="Edit box details"
-            clickHandler={onEditButtonClick}
-          />
-          <Button
-            type="button"
-            textContent="Delete box"
-            clickHandler={onDeleteButtonClick}
-          />
-        </div>
-      )}
+    <BoxAdminViewWrapper>
+      <ButtonsWrapper>
+        <Button
+          status="primary"
+          padding="0.5em"
+          type="button"
+          textContent="Edit box details"
+          clickHandler={onEditButtonClick}
+        />
+        <Button
+          status="red"
+          padding="0.5em"
+          type="button"
+          textContent="Delete box"
+          clickHandler={onDeleteButtonClick}
+        />
+      </ButtonsWrapper>
+
       {showEditModal && !showDeleteModal && (
         <Modal closeModal={onCloseEditModal}>
           <BoxEdit />
         </Modal>
       )}
+
       {showDeleteModal && !showEditModal && (
         <Modal closeModal={onCloseDeleteModal}>
           <BoxDelete />
         </Modal>
       )}
-    </div>
+    </BoxAdminViewWrapper>
   );
 };
 
