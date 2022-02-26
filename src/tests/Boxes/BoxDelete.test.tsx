309b69b9fa60f17dfe802d2bc1522663612ff996
01zulfi <85733202+01zulfi@users.noreply.github.com>
2022-02-26 19:19:45 +0500

    Update BoxDelete.test.tsx

--- src/tests/Boxes/BoxDelete.test.tsx
+++ src/tests/Boxes/BoxDelete.test.tsx
@@ -13,9 +13,7 @@ jest.mock('../../context/BoxContext', () => ({
 }));
 
 jest.mock('react-router-dom', () => ({
-  Link: function Link({ to, children }: { to: string; children: any }) {
-    return <a href={to}>{children}</a>;
-  },
+  useNavigate: () => () => {},
 }));
 
 jest.mock('../../utils/firebase', () => ({
@@ -23,18 +21,6 @@ jest.mock('../../utils/firebase', () => ({
 }));
 
 describe('tests BoxDelete component', () => {
-  it('renders box has been deleted text when button clicks', async () => {
-    render(<BoxDelete />);
-    const deleteButton = screen.getByRole('button');
-
-    userEvent.click(deleteButton);
-
-    const heading = await screen.findByRole('heading', {
-      name: 'the box has been deleted',
-    });
-    expect(heading).toBeInTheDocument();
-  });
-
   it('invokes deleteBox firebase method with box id', async () => {
     render(<BoxDelete />);
     const deleteButton = screen.getByRole('button');
