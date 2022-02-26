61783259266e00ae6fcd47c764a0a6f0aa415617
01zulfi <85733202+01zulfi@users.noreply.github.com>
2022-02-26 15:13:19 +0500

    Style Loading component

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,7 +1,31 @@
 import React, { FC } from 'react';
+import styled from 'styled-components';
 
-const Loading: FC = function Loading() {
-  return <div>Loading...</div>;
+const LoadingWrapper = styled.div`
+  width: ${(props: any) => props.width};
+  aspect-ratio: 1;
+  border-radius: 50%;
+  border: 4px solid ${(props: any) => props.theme.text.four};
+  border-bottom-color: ${(props: any) => props.theme.frost.two};
+
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+
+  animation: 1s linear infinite spin;
+`;
+
+interface LoadingProps {
+  width: string;
+}
+
+const Loading: FC<LoadingProps> = function Loading({ width }) {
+  return <LoadingWrapper width={width} />;
 };
 
 export default Loading;
