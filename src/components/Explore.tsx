3bd2c9091fd70bf31e1c826f6a56cb1bca784787
01zulfi <85733202+01zulfi@users.noreply.github.com>
2022-02-27 19:15:10 +0500

    Add Explore component

---
+++ src/components/Explore.tsx
@@ -0,0 +1,28 @@
+import React, { FC, useState, useEffect } from 'react';
+import firebase from '../utils/firebase';
+import Loading from './Loading';
+
+const Explore: FC = function Explore() {
+  const [boxes, setBoxes] = useState<any>([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const fetchedData = await firebase.getBoxList(1000);
+      setBoxes(fetchedData);
+      setIsLoaded(true);
+    })();
+  }, []);
+
+  return (
+    <section>
+      {isLoaded ? (
+        boxes.map((box: any) => <div>{box.name}</div>)
+      ) : (
+        <Loading width="50px" />
+      )}
+    </section>
+  );
+};
+
+export default Explore;
