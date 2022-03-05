35ba8b5722c51ccd780b73d85d75b97325448b18
01zulfi <85733202+01zulfi@users.noreply.github.com>
2022-03-05 16:45:47 +0500

    Style Overview.tsx

--- src/components/Profile/Overview.tsx
+++ src/components/Profile/Overview.tsx
@@ -1,5 +1,16 @@
 import React, { FC } from 'react';
+import styled from 'styled-components';
 import localDateFromMilliseconds from '../../utils/local-date-from-milliseconds';
+
+const OverviewWrapper = styled.section`
+  background: ${(props: any) => props.theme.base.two};
+  display: flex;
+  flex-direction: column;
+  gap: 0.4em;
+  padding: 1em;
+  border-radius: 5px;
+  margin: 1em 0em;
+`;
 
 interface OverviewProps {
   user: any;
@@ -11,13 +22,13 @@ const Overview: FC<OverviewProps> = function Overview({ user }) {
 
   /* eslint-disable react/jsx-one-expression-per-line */
   return (
-    <section>
+    <OverviewWrapper>
       <div>Joined on {localDateFromMilliseconds(time)}</div>
       <div>Admin at {adminBoxes.length} boxes.</div>
       <div>Member at {joinedBoxes.length} boxes.</div>
       <div>Authored {authoredStrings.length} strings.</div>
       <div>Authored {authoredKnots.length} knots.</div>
-    </section>
+    </OverviewWrapper>
   );
 };
 
