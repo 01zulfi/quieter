9c8a09a763517c2288b6316a246a9430d87ea286
01zulfi <85733202+01zulfi@users.noreply.github.com>
2022-03-05 13:40:45 +0500

    Start styling Profile.tsx

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,10 +1,40 @@
 import React, { FC, useEffect, useState } from 'react';
+import styled from 'styled-components';
 import { useParams } from 'react-router-dom';
+import Avatar from './Avatar';
 import { useUser } from '../../context/UserContext';
 import firebase from '../../utils/firebase';
 import Loading from '../Loading';
 import ProfileEditableView from './ProfileEditableView';
 import ProfileContext from '../../context/ProfileContext';
+
+const ProfileWrapper = styled.section`
+  width: 65%;
+  margin: 2em auto;
+
+  @media (max-width: 680px) {
+    width: 95%;
+  }
+`;
+
+const UsernameAndAvatarWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 1em;
+  height: 4em;
+  margin-top: 1em;
+`;
+
+const UsernameWrapper = styled.h3`
+  text-decoration: underline;
+`;
+
+const Line = styled.div`
+  height: 0.1em;
+  padding-top: 0.5em;
+  background: ${(props: any) => props.theme.base.four};
+`;
 
 const Profile: FC = function Profile() {
   const params = useParams();
@@ -33,15 +63,22 @@ const Profile: FC = function Profile() {
   if (!isLoaded) return <Loading width="35px" />;
 
   return (
-    <div>
+    <ProfileWrapper>
       {isCurrentUserProfile && (
         <ProfileContext.Provider value={user}>
           <ProfileEditableView />
         </ProfileContext.Provider>
       )}
 
-      <h3>{user.username}</h3>
-    </div>
+      <UsernameAndAvatarWrapper>
+        <Avatar userId={user.id} />
+        <UsernameWrapper>
+          {isCurrentUserProfile ? signedInUser.username : user.username}
+        </UsernameWrapper>
+      </UsernameAndAvatarWrapper>
+
+      <Line />
+    </ProfileWrapper>
   );
 };
 
